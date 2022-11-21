2d8ced997df7e0bcfac24e6daf93678500c8a5b1
Muhammad Jafar Ash Siddiq <mjafarashsiddiq@gmail.com>
2022-11-21 21:04:52 +0700

    Initial Commit

---
+++ CoffeFinder/CoffeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeFinderApp.swift
+//  CoffeFinder
+//
+//  Created by Muhammad Jafar Ash Siddiq on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
